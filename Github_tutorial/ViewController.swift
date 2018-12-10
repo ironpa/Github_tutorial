6a6bb3465ff7c56f88768c4642aa64ba3e61995f
ironpa <ireneusz.parysz@gmail.com>
2018-12-10 19:47:53 +0100

    Initial Commit

---
+++ Github_tutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Github_tutorial
+//
+//  Created by Ireneusz Parysz on 10/12/2018.
+//  Copyright © 2018 Ireneusz Parysz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
