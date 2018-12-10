69b0adbea2e3ae9e1e7045800957cba99ee3d8ca
ironpa <ireneusz.parysz@gmail.com>
2018-12-10 20:46:49 +0100

    Zmienione cos jeszcze

--- Github_tutorial/ViewController.swift
+++ Github_tutorial/ViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class ViewController: UIViewController {
     
 //    Testowe zmiany pod git'a
+    
+//    I zmienione cos jeszcze
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
